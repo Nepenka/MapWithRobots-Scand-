1959cfa2ee39f5278a2b9812423717bccd282951
Vlad Perelygin <vladmafia69@gmail.com>
2024-02-28 23:16:41 +0300

    28.02.2024

--- MapWithRobots(Scand)/ViewController.swift
+++ MapWithRobots(Scand)/ViewController.swift
@@ -112,22 +112,11 @@ class ViewController: UIViewController {
     @objc func startButtonAction() {
         let initialCoordinate = Coordinate(x: 2, y: 1)
         let initialDirection = Direction.up
-        let warehouse = Warehouse(dimensions: (width: 10 , height: 8), entrance: (x: 2, y: 0), exit: (x: 9, y: 5), obstacles: [(x: 2, y: 3), (x: 5, y: 5), (x:4, y: 1), (x: 5, y: 6)].map { Partition(x: $0.x, y: $0.y) }, partitions: [
-            (x: 0, y: 0), (x: 0, y: 1), (x: 0, y: 2), (x: 0, y: 3),
-            (x: 0, y: 4), (x: 0, y: 5), (x: 0, y: 6), (x: 0, y: 7),
-            (x: 9, y: 0), (x: 9, y: 1), (x: 9, y: 2), (x: 9, y: 3),
-            (x: 9, y: 4), (x: 9, y: 5), (x: 9, y: 6), (x: 9, y: 7),
-            (x: 1, y: 0), (x: 2, y: 0), (x: 3, y: 0), (x: 4, y: 0),
-            (x: 5, y: 0), (x: 6, y: 0), (x: 7, y: 0), (x: 8, y: 0),
-            (x: 1, y: 7), (x: 2, y: 7), (x: 3, y: 7), (x: 4, y: 7),
-            (x: 5, y: 7), (x: 6, y: 7), (x: 7, y: 7), (x: 8, y: 7)
-        ].map { Partition(x: $0.x, y: $0.y) }, boxes: [
-            Box(id: 1, position: Box.Position(x: 3, y: 3)),
-            Box(id: 2, position: Box.Position(x: 4, y: 5)),
-            Box(id: 3, position: Box.Position(x: 6, y: 3))
-        ])
+        
+        let warehouse = createWarehouse()
+        
         var map = Map(warehouse: warehouse)
-        var robots: [Robot] = []
+        let robots: [Robot] = []
         
         let robot = Robot(coordinate: initialCoordinate, direction: initialDirection, robotID: 1)
         
